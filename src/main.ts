4aa953f4f2a12083b0d03219c19f6b7623f4e33b
WwwXxxBo <2606489372@qq.com>
2024-10-10 12:32:02 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// 引入 Pinia 持久化组件
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
+// 引入 UI 库
+import ArcoVue from '@arco-design/web-vue'
+import '@arco-design/web-vue/dist/arco.css'
+import ArcoVueIcon from '@arco-design/web-vue/es/icon'
+// 引入国际化组件
+import i18n from "@/i18n"
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
-
-app.use(createPinia())
+app.use(ArcoVueIcon)
+app.use(ArcoVue);
+app.use(i18n)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')

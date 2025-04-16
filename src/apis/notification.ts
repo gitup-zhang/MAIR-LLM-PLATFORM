f4b5825abfddd52936f899a483f758ae16a51c2c
WwwXxxBo <2606489372@qq.com>
2025-04-16 22:55:51 +0800

    提交代码

--- src/apis/notification.ts
+++ src/apis/notification.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { httpInstance } from '@/utils/http-util'
 
 // 获取课程通知列表
@@ -47,54 +46,4 @@ export const createNotification = (form: object) => {
     },
     withCredentials: true,
   });
-=======
-import { httpInstance } from '@/utils/http-util'
-
-// 获取课程通知列表
-export const getNotificationList = (
-  classId: number,
-  searchText: string,
-  page: number,
-  count: number
-) => {
-  return httpInstance({
-    url: "/announce/",
-    method: "GET",
-    params: {
-      class_id: classId,
-      content: searchText,
-      page: page,
-      count: count
-    },
-    headers: {
-      "content-type": "application/json",
-    },
-    withCredentials: true,
-  });
-};
-
-// 删除课程通知列表
-export const deleteNotification = (id: number) => {
-  return httpInstance({
-    url: "/announce/" + id + '/',
-    method: "DELETE",
-    headers: {
-      "content-type": "application/json",
-    },
-    withCredentials: true,
-  });
-};
-
-// 创建课程通知
-export const createNotification = (form: object) => {
-  return httpInstance({
-    url: "/announce/",
-    method: "POST",
-    data: JSON.stringify(form),
-    headers: {
-      "content-type": "application/json",
-    },
-    withCredentials: true,
-  });
->>>>>>> c0bb64e9aff82d110ad8814df620b5d8587b89cd
 };
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineStore } from 'pinia'
 
 // 定义 User 状态
@@ -20,27 +19,4 @@ export const useUserStore = defineStore({
     createTime: 1,
     modifyTime: 1,
   }),
-=======
-import { defineStore } from 'pinia'
-
-// 定义 User 状态
-export const useUserStore = defineStore({
-  id: 'user',
-  state: () => ({
-    // 用户信息
-    id: 0,
-    nickname: '',
-    avatar: '',
-    stuId: '',
-    name: '',
-    email: '',
-    phone: '',
-    roleId: 2,
-    role: '',
-    personId: '',
-    areaId: 1,
-    createTime: 1,
-    modifyTime: 1,
-  }),
->>>>>>> c0bb64e9aff82d110ad8814df620b5d8587b89cd
 })
